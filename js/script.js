ffacb1143fbfdc9d873bf7b40d42195b1477cd89
lionel3107 <dlmsonelterror@gmail.com>
2024-03-22 14:17:58 +0000

    deuxième mise à jour de mon portfolio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
  var typed = new Typed(".typing",{
-    strings:["","Web Designer", "Web developer", "Graphic Designer", "Farmer Troup's Child"],
+    strings:["Web Designer", "Web and Mobile developer", "Graphic Designer", "Backend Developer", "Freelancer", "Content Creator"], 
     typeSpeed:100,
     backSpeed:60,
     loop:true,
